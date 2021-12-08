07352fc5dc9af7ea373cd0498104820cb31b4026
Kaitlyn Goodall <taswyn@gmail.com>
2021-12-08 13:56:05 -0700

    Updating Tooling pre-routing work

--- component-app2/vite.config.js
+++ component-app2/vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    '@' : path.resolve(__dirname, './src')
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.js'),
